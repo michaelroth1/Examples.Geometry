30c166f558b8ef577dd1c09539b64afadca1bbad
mitoSoft <michael.mr.roth@gmail.com>
2023-12-10 11:59:55 +0100

    update Main

--- CalcApp/Program.cs
+++ CalcApp/Program.cs
@@ -7,21 +7,18 @@ internal class Program
 {
     static void Main()
     {
-        Console.WriteLine($"Start:");
-
         Rectangle rectangle = new(2, 4);
         CalculateRectangle(rectangle);
-
-        Console.ReadLine();
     }
 
     private static void CalculateRectangle(Rectangle rectangle)
     {
+        Console.WriteLine($"Calculating the area of a {rectangle.GetType().Name}:");
         Console.WriteLine($"The {rectangle.GetType().Name}'s "
            + $"width, height is {rectangle.Width}, {rectangle.Height}.");
 
         var area = (new AreaCalulator()).CalculateArea(rectangle);
 
-        Console.WriteLine($"The {rectangle.GetType().Name}'s area is {area}.");
+        Console.WriteLine($"The {rectangle.GetType().Name}'s area is {area}.{Environment.NewLine}");
     }
 }
